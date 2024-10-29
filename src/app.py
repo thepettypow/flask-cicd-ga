ce7b2d227b5e26646c4b3998a52c0bd4332baee0
petty pow <petty@pow.local>
2024-10-29 18:59:13 +0330

    nothing

--- src/app.py
+++ src/app.py
@@ -1,11 +1,12 @@
 from flask import Flask
 
+
 app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Hi From Flask"
-
+    return "<h1>Hi From Flask</h1>"
 
 if __name__ == "__main__":
     app.run()
+for i in ra
